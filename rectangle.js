61b5b7cc40d6387d5cee45c80de06b56d0bd5766
wangding <408542507@qq.com>
2019-03-27 15:34:34 +0800

    计算周长和面积1.0

--- rectangle.js
+++ rectangle.js
@@ -1,13 +1,59 @@
 $(function() {
-    var $width=$('#width'),$height=$('#height'),$btnCal=$('#calcute'),$perimeter=$('#perimeter'),$area=$('#area');
+    var $width = $('#rectangle-width'),
+        $height = $('#rectangle-height'),
+        $btnCal = $('#rectangle-calc'),
+        $perimeter = $('#rectangle-perimeter'),
+        $area = $('#rectangle-area'),
+        blnValid = false;
+
     $btnCal.click(function(){
-          var w=Number($width.val()),
-              h=Number($height.val());
-        var p=2*(w+h),
-              a=w*h; 
+        if(!blnValid) return;
+
+        var width  = Number($width.val()),
+            height = Number($height.val()),
+            p = roundFractional(width * 2 + height * 2, 2),
+            a = roundFractional(width * height, 2);
         $perimeter.val(p);
-            $area.val(a);
-              
+        $area.val(a);           
     });
+ 
+    $width.focusout(function() {
+        blnValid = validate('#rectangle-width');     
+    });
+
+    $height.focusout(function() {
+        blnValid = validate('#rectangle-height');
+    });
+
+    function roundFractional(x, n) {
+        return Math.round(x * Math.pow(10, n)) / Math.pow(10, n);
+    }
+
+    function validate(field) {
+        var $data    = $(field),
+            $message = $(field + '-validate'),
+            label    = $(field).attr('data-label');
+       
+        if($data.val() === '') {
+            $message.html(label + '不能为空！');
+            $data.select();
+            return false;                    
+        }
+
+        if(!/^-?(0|[1-9]\d*)(\.\d*)?([eE][+-]?\d+)?$/.test($data.val())) {
+            $message.html(label + '必须是数值');
+            $data.select();
+            return false;                       
+        }
+
+        if(window.Number($data.val()) < 0) {
+            $message.html(label + '必须大于零');
+            $data.select();
+            return false;                    
+        }
+
+        $message.html('');
+        return true;
+    }
 
 });
