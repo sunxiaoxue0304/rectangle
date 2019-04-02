6ca4182874bea3a6ef4a8625f7478f0e6ba64928
wangding <408542507@qq.com>
2019-04-02 14:44:11 +0800

    v0.5

--- rectangle.js
+++ rectangle.js
@@ -1,11 +1,11 @@
 /* global Rectangle: true */
 $(function() {
     var $width=$('#width'),
-    $height=$('#height'),
-    $btnCal=$('#calcute'), 
-    $perimeter=$('#perimeter'),
-    $area=$('#area');
-    $btnCal.click(function(){
+        $height=$('#height'),
+        $btnCal=$('#calculate'), 
+        $perimeter=$('#perimeter'),
+        $area=$('#area');
+        $btnCal.click(function(){
           var w = $width.val(),
               h = $height.val();
         var r = new Rectangle(w,h);
