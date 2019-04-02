1d858abd3118b5c308939495918b19e3e9b67d3c
wangding <408542507@qq.com>
2019-04-02 15:26:14 +0800

    v1.0

--- rectangle.js
+++ rectangle.js
@@ -1,16 +1,43 @@
-/* global Rectangle: true */
+/* global Rectangle, validate: true */
 $(function() {
     var $width=$('#width'),
         $height=$('#height'),
         $btnCal=$('#calculate'), 
         $perimeter=$('#perimeter'),
-        $area=$('#area');
+        $area=$('#area'),
+        $widthValidate = $('#width-validate'),
+        $heightValidate = $('#height-validate'),
+        isPassValidate = false;
+
+    $width.focusout(function() {
+      var result = validate($width.val());
+          isPassValidate = result.isOK;
+          if(!result.isOK) {
+              $widthValidate.html('宽度' + result.reason);
+              $width.select();                           
+          } else {
+              $widthValidate.html('');                      
+          }           
+    });
+
+    $height.focusout(function() {
+      var result = validate($height.val());
+          isPassValidate = result.isOK;
+          if(!result.isOK) {
+              $heightValidate.html('高度' + result.reason);
+              $height.select();                  
+          } else {
+              $heightValidate.html('');                 
+          }
+    });
+
         $btnCal.click(function(){
+          if(!isPassValidate) return;
           var w = $width.val(),
               h = $height.val();
-        var r = new Rectangle(w,h);
-        $perimeter.val(r.perimeter());
-            $area.val(r.area());
+          var r = new Rectangle(w,h);
+          $perimeter.val(r.perimeter());
+          $area.val(r.area());
               
     });
 
